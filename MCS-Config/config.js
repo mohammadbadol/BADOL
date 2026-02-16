c65d2bd9a06c7c56cd314f674a4be4168e127539
MOHAMMAD BADOL <mohammadbadol187@gmail.com>
2026-02-16 19:06:54 +0600

    Create config.js

---
+++ MCS-Config/config.js
@@ -0,0 +1,34 @@
+Module.exports = {
+  BOT_TOKEN: "YOUR-BOT-TOKEN-HERE",
+
+  BOT_SETTINGS: {
+    NAME: "𝄞⋆⃝🧚‍𝐌𝐂𝐒-𝐁𝐎𝐓🧚‍⋆⃝𝄞",
+    VERSION: "2.5.0-VIP",
+    PREFIX: "/",
+    ADMINS: ["6954597258"],
+    CREDIT: "MOHAMMAD BADOL",
+    FOOTER: "© 𝟐𝟎𝟐𝟔 𝐌𝐂𝐒 𝐁𝐎𝐓 𝐏𝐑𝐎𝐉𝐄𝐂𝐓"
+  },
+
+  OWNER: {
+    ID: 6954597258, // [WARNING] CRITICAL ID: DO NOT CHANGE! If this ID is modified, the bot's core authentication will fail and it will stop working.
+    NAME: "MOHAMMAD BADOL",
+    USERNAME: "B4D9L_007"
+  },
+
+  SOCIAL: {
+    FACEBOOK: "https://www.facebook.com/B4D9L",
+    WHATSAPP: "https://wa.me/+8801782721761",
+    TELEGRAM: "https://t.me/mreditorzone"
+  },
+
+  DESIGN: {
+    LINE: "━━━━━━━━━━━━━━━━━━━━━━━━",
+    TICK: "✅",
+    ERROR: "❌",
+    INFO: "💡",
+    STAR: "⭐"
+  },
+
+  REQUIRED_CHATS: []
+};
